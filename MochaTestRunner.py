5e77fc0dd532abc7a03259a0c644dc08a5f71ae9
Bijoy Thomas <bijoyjth@gmail.com>
2015-12-14 10:45:11 -0600

    Ability to run previous mocha command

--- MochaTestRunner.py
+++ MochaTestRunner.py
@@ -7,6 +7,9 @@ import functools
 import re
 
 class MochaTestRunnerCommand(sublime_plugin.TextCommand):
+
+  state = {}
+
   def clear_test_view(self):
     self.output_view.set_read_only(False)
     edit = self.output_view.begin_edit()
@@ -17,10 +20,10 @@ class MochaTestRunnerCommand(sublime_plugin.TextCommand):
   def show_tests_panel(self):
     if not hasattr(self, 'output_view'):
       hasattr(self, 'output_view')
-      self.output_view = self.view.window().get_output_panel('tests')
+      self.output_view = self.view.window().get_output_panel('mocha_tests')
 
     self.clear_test_view()
-    self.view.window().run_command('show_panel', {'panel': 'output.tests'})
+    self.view.window().run_command('show_panel', {'panel': 'output.mocha_tests'})
 
   def read_stdout(self):
     while True:
@@ -39,17 +42,24 @@ class MochaTestRunnerCommand(sublime_plugin.TextCommand):
     self.output_view.end_edit(edit)
     self.output_view.set_read_only(True)
 
-  def run(self, edit):
+  def run(self, edit, **args):
     selection = self.view.sel()[0]
     line = self.view.line(selection.b)
     line_text = self.view.substr(line)
-    print 'In here'
-    self.view.window().run_command('show_panel', {'panel': 'output.tests'})
     mocha_command = 'mocha -R tap'
-    if line_text.find('describe') != -1 or line_text.find('it') != -1:
-      mocha_command = mocha_command + ' --grep ' + re.search("'(.*)'", line_text).group()
-    mocha_command = mocha_command + ' ' + self.view.file_name()
+
+    if 'previous' in args:
+      print 'Using previous command'
+      mocha_command = MochaTestRunnerCommand.state.get(self.view.window().id(), {}).get('command')
+    elif 'all_tests' in args:
+      mocha_command = mocha_command + ' ' + self.view.file_name()
+    elif line_text.find('describe') != -1 or line_text.find('it') != -1:
+      mocha_command = mocha_command + ' --grep ' + re.search("'(.*)'", line_text).group() + ' ' + self.view.file_name()
+    else:
+      mocha_command = mocha_command + ' ' + self.view.file_name()
+
     print 'Running ' + mocha_command
+    MochaTestRunnerCommand.state.setdefault(self.view.window().id(), {})['command'] = mocha_command
     self.show_tests_panel()
     self.proc = subprocess.Popen(mocha_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     thread.start_new_thread(self.read_stdout, ())
