7791ec28ee2f03800fadb83c9236d295f4592f14
Bijoy Thomas <bijoyjth@gmail.com>
2016-03-18 15:09:14 -0500

    Update plugin fo ST3

--- MochaTestRunner.py
+++ MochaTestRunner.py
@@ -1,9 +1,6 @@
 import sublime
 import sublime_plugin
 import subprocess
-import thread
-import os
-import functools
 import re
 
 class MochaTestRunnerCommand(sublime_plugin.TextCommand):
@@ -12,44 +9,33 @@ class MochaTestRunnerCommand(sublime_plugin.TextCommand):
 
   def clear_test_view(self):
     self.output_view.set_read_only(False)
-    edit = self.output_view.begin_edit()
+    edit = self.edit
     self.output_view.erase(edit, sublime.Region(0, self.output_view.size()))
-    self.output_view.end_edit(edit)
     self.output_view.set_read_only(True)
 
   def show_tests_panel(self):
     if not hasattr(self, 'output_view'):
       hasattr(self, 'output_view')
-      self.output_view = self.view.window().get_output_panel('mocha_tests')
 
+    self.output_view = self.view.window().get_output_panel('mocha_tests')
     self.clear_test_view()
     self.view.window().run_command('show_panel', {'panel': 'output.mocha_tests'})
 
-  def read_stdout(self):
-    while True:
-      data = os.read(self.proc.stdout.fileno(), 32768)
-      if data != '':
-        sublime.set_timeout(functools.partial(self.append_data, self.proc, data), 0)
-        continue
-      else:
-        self.proc.stdout.close()
-        break
-
-  def append_data(self, proc, data):
+  def append_data(self, data):
     self.output_view.set_read_only(False)
-    edit = self.output_view.begin_edit()
+    edit = self.edit
     self.output_view.insert(edit, self.output_view.size(), data)
-    self.output_view.end_edit(edit)
     self.output_view.set_read_only(True)
 
   def run(self, edit, **args):
+    self.edit = edit
     selection = self.view.sel()[0]
     line = self.view.line(selection.b)
     line_text = self.view.substr(line)
     mocha_command = 'mocha -R tap'
 
     if 'previous' in args:
-      print 'Using previous command'
+      print('Using previous command')
       mocha_command = MochaTestRunnerCommand.state.get(self.view.window().id(), {}).get('command')
     elif 'all_tests' in args:
       mocha_command = mocha_command + ' ' + self.view.file_name()
@@ -58,8 +44,8 @@ class MochaTestRunnerCommand(sublime_plugin.TextCommand):
     else:
       mocha_command = mocha_command + ' ' + self.view.file_name()
 
-    print 'Running ' + mocha_command
-    MochaTestRunnerCommand.state.setdefault(self.view.window().id(), {})['command'] = mocha_command
     self.show_tests_panel()
-    self.proc = subprocess.Popen(mocha_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    thread.start_new_thread(self.read_stdout, ())
+    print('Running ' + mocha_command)
+    output = subprocess.getoutput(mocha_command)
+    MochaTestRunnerCommand.state.setdefault(self.view.window().id(), {})['command'] = mocha_command
+    self.append_data(output)
